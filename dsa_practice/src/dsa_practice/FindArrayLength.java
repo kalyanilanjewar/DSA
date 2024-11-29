ab5252b877672e2975b91c3835d1a38dd0fa0db0
Kalyani <kblanjewar024@gmail.com>
2024-11-30 02:17:43 +0530

    lengthOfArray using length property

---
+++ dsa_practice/src/dsa_practice/FindArrayLength.java
@@ -0,0 +1,16 @@
+package dsa_practice;
+
+public class FindArrayLength 
+{
+	public static void main(String[] args)
+	{
+		//Initilize an array with integers
+		int[] array = {67, 23, 8, 1, 6, 9, 0, 5};
+		
+		// get the length of array using length property
+		int lengthOfArray = array.length;
+		
+		//print the length of array
+		System.out.println("length of array is: "+lengthOfArray);
+	}
+}
